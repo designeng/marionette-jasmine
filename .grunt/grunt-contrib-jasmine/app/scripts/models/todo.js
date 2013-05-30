654493ad11cc081d08f602ad31ac6fbe15ffedca
designeng <denissavenok@gmail.com>
2013-05-30 19:02:37 +0400

    removed .grunt folder

--- .grunt/grunt-contrib-jasmine/app/scripts/models/todo.js
+++
@@ -1,6 +0,0 @@
-if (typeof __coverage__ === 'undefined') { __coverage__ = {}; }
-if (!__coverage__['./app/scripts/models/todo.js']) {
-   __coverage__['./app/scripts/models/todo.js'] = {"path":"./app/scripts/models/todo.js","s":{"1":0,"2":0,"3":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"(anonymous_1)","line":1,"loc":{"start":{"line":1,"column":21},"end":{"line":1,"column":40}}}},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":8,"column":3}},"2":{"start":{"line":2,"column":2},"end":{"line":6,"column":5}},"3":{"start":{"line":7,"column":2},"end":{"line":7,"column":14}}},"branchMap":{}};
-}
-var __cov_XWdgztg4GIL_b_tZkR1hmw = __coverage__['./app/scripts/models/todo.js'];
-__cov_XWdgztg4GIL_b_tZkR1hmw.s['1']++;define(['backbone'],function(Backbone){__cov_XWdgztg4GIL_b_tZkR1hmw.f['1']++;__cov_XWdgztg4GIL_b_tZkR1hmw.s['2']++;var todo=Backbone.Model.extend({defaults:{title:'some title'}});__cov_XWdgztg4GIL_b_tZkR1hmw.s['3']++;return todo;});
--- .grunt/grunt-contrib-jasmine/app/scripts/templates.js
+++
@@ -1,6 +0,0 @@
-if (typeof __coverage__ === 'undefined') { __coverage__ = {}; }
-if (!__coverage__['./app/scripts/templates.js']) {
-   __coverage__['./app/scripts/templates.js'] = {"path":"./app/scripts/templates.js","s":{"1":0,"2":0,"3":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"(anonymous_1)","line":3,"loc":{"start":{"line":3,"column":7},"end":{"line":3,"column":25}}}},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":1,"column":13}},"2":{"start":{"line":3,"column":0},"end":{"line":12,"column":3}},"3":{"start":{"line":5,"column":4},"end":{"line":11,"column":6}}},"branchMap":{}};
-}
-var __cov_YX5msQYQIL$x6TO4o3S4pQ = __coverage__['./app/scripts/templates.js'];
-__cov_YX5msQYQIL$x6TO4o3S4pQ.s['1']++;'use strict';__cov_YX5msQYQIL$x6TO4o3S4pQ.s['2']++;define(function(require){__cov_YX5msQYQIL$x6TO4o3S4pQ.f['1']++;__cov_YX5msQYQIL$x6TO4o3S4pQ.s['3']++;return{extendedTemplateParamsTemplate:require('tpl!../templates/testTT.html')};});
